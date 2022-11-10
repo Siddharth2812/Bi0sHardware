705b473221a44f35719677ec74c98eb5d37092bf
Siddharth Reddy Anthireddy <sid128@pop-os.localdomain>
2022-11-10 21:43:04 +0530

    Add base Files

---
+++ src/data/Cofounder.ts
@@ -0,0 +1,24 @@
+const Cofounders =  [
+    {
+        firstname: 'Arvind',
+        lastname: 'S Raj',
+        affiliation: 'Arizona State University'
+    },
+    {
+        firstname: 'Reno',
+        lastname: 'Robert',
+        affiliation: 'Zero Day Initiative'
+    },
+    {
+        firstname: 'Sreepriya',
+        lastname: 'Chalakkal',
+        affiliation: 'Siemens'
+    },   
+    {
+        firstname: 'Sreepriya',
+        lastname: 'Chalakkal',
+        affiliation: 'Siemens'
+    }
+];
+
+export default Cofounders;
