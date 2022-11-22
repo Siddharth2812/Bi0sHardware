e3ac9c4065d1fd898db70fce20a39fe65cfdc63f
Siddharth <sid128@pop-os.localdomain>
2022-11-22 22:16:28 +0530

    debug 4

--- src/landing/index.tsx
+++ src/landing/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Layout from "../layout";
 import LandingHeader from "./header";
-import AboutUs from "./content";
-import FieldsofResearch from "./fields";
-
+// import AboutUs from "./content";
 const LandingPage = () => (
     <Layout title="Team bi0s - India's No.1 Ranked CTF Team & Cyber Security Research Club">
         <LandingHeader />
